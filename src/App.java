fb15216173dfb4c59778d8d045d1471613e8d356
Logan Stach <loganjeffreystach@gmail.com>
2022-03-22 17:44:47 -0400

    3-22-22 Edits
    Started working on generating rows and columns of truth table based on # of inputs

--- src/App.java
+++ src/App.java
@@ -3,7 +3,5 @@ public class App {
     public static void main(String[] args) {
 
         new MyFrame();
-
-
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class MyFrame extends JFrame implements ActionListener {
 
@@ -9,23 +10,26 @@ public class MyFrame extends JFrame implements ActionListener {
     JButton btnHelp;
     JButton btnGo;
     JTextField txtEntry;
+    JPanel headerPanel;
+    JPanel mainPanel;
 
     MyFrame() {
 
         this.setTitle("Truth Table Generator");
-        this.setDefaultCloseOperation(3); //makes close-button work. not sure if needed
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE); //makes close-button work. not sure if needed
         this.setLayout((new BorderLayout()));
         this.setSize(750, 520);
 
         //instantiates header and main content panels
-        JPanel headerPanel = new JPanel();
-        JPanel mainPanel = new JPanel();
+        headerPanel = new JPanel(); //these need to be made global
+        mainPanel = new JPanel();
 
         //adds content to header panel
         //TODO: organize headerPanel to ensure entry is centered and extends with app window
 
         headerPanel.setBackground(Color.pink);
         headerPanel.setLayout(new FlowLayout());
+        headerPanel.setPreferredSize(new Dimension(10, 75));
 
         lblDisplay = new JLabel();
         lblDisplay.setText("Enter a formula:");
@@ -60,7 +64,78 @@ public class MyFrame extends JFrame implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == btnHelp) {
-            System.out.println("Help");
+            new HelpFrame();
+        } else if (e.getSource() == btnGo) {
+            ParseInput();
+            //calculate formula
         }
+
+    }
+
+    public void ParseInput() {
+
+        /*tasks:
+        1. check for parentheses
+        2. check for variables
+        3. check for operators
+         */
+
+        ArrayList<String> validVars = new ArrayList<>();
+        ArrayList<String> validOperators = new ArrayList<>();
+
+        validVars.add("a");
+        validVars.add("b");
+        validVars.add("c");
+        validVars.add("d");
+
+        validVars.add("e");
+        validVars.add("g");
+        validVars.add("h");
+        validVars.add("i");
+
+        validVars.add("j");
+        validVars.add("k");
+        validVars.add("l");
+        validVars.add("m");
+
+        validVars.add("n");
+        validVars.add("o");
+        validVars.add("p");
+        validVars.add("q");
+
+        validVars.add("r");
+        validVars.add("s");
+        validVars.add("u");
+        validVars.add("v");
+
+        validVars.add("w");
+        //validVars.add("x");
+        validVars.add("y");
+        validVars.add("z");
+
+        //hashmap where key is variable letter and value is boolean, boolean is true
+        //if corresponding letter has been counted and false if not?
+        //HashMap<String, Boolean> varMap = new HashMap<>();
+
+        String strInput = txtEntry.getText().toLowerCase();
+        ArrayList<String> inputVars = new ArrayList<>();
+
+        for (int i = 0; i < strInput.length(); i++) {
+            if (validVars.contains(strInput.substring(i, i + 1))) { //check if given character is a valid variable
+                if (!inputVars.contains(strInput.substring(i, i + 1))) { //checks if already in list of input variables
+                    inputVars.add(strInput); //adds given variable if not already in list (avoids double-counting)
+                }
+            }
+        }
+
+        int numUniqueVariables = inputVars.size();
+
+        int columns = numUniqueVariables + 1;
+        int rows = (int) Math.pow(2, numUniqueVariables) + 1;
+
+        GridLayout truthGrid = new GridLayout(rows, columns);
+
+        mainPanel.setLayout(truthGrid);
+
     }
 }
